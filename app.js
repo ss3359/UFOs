cb5fea6bc93035533c8e705748bc6625f1bc9f84
ss3359 <104328106+ss3359@users.noreply.github.com>
2022-08-17 08:09:02 -0600

    I am building my website

--- app.js
+++ app.js
@@ -53,6 +53,42 @@ function buildTable(data)
 
 }
 
+   // Basic Javascript IF-STATEMENT
+    // if(condition){code to execute}
+
+    //Psudocode practice 
+    // if(a date is entered){ Filter the default data to show only the date entered  };
+    
+
+function handleClick()
+{
+    let date = d3.select("#datetime").property("value"); 
+    let filterData = tableData; 
+    // Check to see if a date was entered and filter the data using that date
+
+    if(date)
+    {
+        // Apply 'filter' to the table data to only keep 
+        // the rows where the 'datetime' value matches the filter value
+        filteredData = filterData.filter(row => row.datetime === date); 
+    }; 
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filterData will
+    // just be the original tableData. 
+
+    buildTable(filteredData); 
+
+
+
+
+}; 
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick); 
+
+// Build the table when the page loads
+buildTable(tableData); 
+
+
 
 
  
